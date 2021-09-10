dd8733f0159529fce2916fed7fff68c13fbb6266
Tarasus <moskaliuktrs@gmail.com>
2021-09-10 12:34:23 +0300

    Initial Commit

---
+++ pipes_gui/pipes_guiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pipes_guiApp.swift
+//  pipes_gui
+//
+//  Created by admin on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct pipes_guiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
